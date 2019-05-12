982333726f0673e3fc92e08490f8600c4ef068e2
wangwei <wangwei35422@163.com>
2019-05-13 00:03:16 +0800

    add: init pages book-detail

--- pages/book-detail/book-detail.js
+++ pages/book-detail/book-detail.js
@@ -1,18 +1,46 @@
-// pages/book-detail/book-detail.js
+import { BookModel } from '../../models/book.js'
+
+const bookModel = new BookModel()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    comments: [],
+    book: null,
+    likeStatus: false,
+    likeCount: 0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    // id
+    const bid = options.bid
+    const detail = bookModel.getDetail(bid)
+    const comments = bookModel.getComments(bid)
+    const likeStatus = bookModel.getLikeStatus(bid)
 
+    detail.then(res => {
+      this.setData({
+        book: res
+      })
+    })
+
+    comments.then(res => {
+      this.setData({
+        comments: res.comments
+      })
+    })
+
+    likeStatus.then(res => {
+      this.setData({
+        likeStatus: res.like_status,
+        likeCount: res.fav_nums
+      })
+    })
   },
 
   /**
